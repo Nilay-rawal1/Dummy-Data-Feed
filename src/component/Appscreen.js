44faf76ed4c043602f55ffb59930097b17d99b57
Nilay <rawalnilay1@gmail.com>
2023-10-19 21:42:30 +0530

    data feed

--- src/component/Appscreen.js
+++ src/component/Appscreen.js
@@ -11,7 +11,7 @@ function Screen() {
     }, []);
 
     useEffect(() => {
-        document.title = 'Task 4';
+        document.title = 'Dummy Data';
     }, []);
     
     const mapping = () => {
